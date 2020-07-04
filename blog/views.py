6ba985e38c737955ece2944bbe5d75a8f2378a95
Adereti Adebobola <emmanueladebobola132@gmail.com>
2020-07-04 14:43:24 +0100

    First commit

---
+++ blog/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render, get_object_or_404
+from .models import Blog
+
+def all_blogs(requests):
+    blogs = Blog.objects.order_by('-date')
+    return render(requests, "blog/all_blogs.html", {'blogs':blogs})
+
+def detail(requests, blog_id):
+    blog = get_object_or_404(Blog, pk=blog_id)
+    return render(requests, 'blog/detail.html', {'blog':blog})
